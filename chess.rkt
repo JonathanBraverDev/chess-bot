5fe2ba5e91ab6e2a3fc243ac87d4134e7d132595
Jonathan Braver <enatanb@gmail.com>
2019-01-16 12:30:07 +0200

    small changes
    i'll work on the pawn last, it's the most complicated piece in the entire game...
    what a cruel joke (its the weakest one)

--- chess.rkt
+++ chess.rkt
@@ -1,16 +1,14 @@
 #lang racket
 
-;(define WR '(W R))
 
-
-(define B1 '((WR WH WB WQ WK WB WH WR)
-             (WP WP WP WP WP WP WP WP)
-             (-- -- -- -- -- -- -- --)
-             (-- -- -- -- -- -- -- --)
-             (-- -- -- -- -- -- -- --)
-             (-- -- -- -- -- -- -- --)
-             (BP BP BP BP BP BP BP BP)
-             (BR BH BB BQ BK BB BH BR)))
+(define B1 '(("WR" "WH" "WB" "WQ" "WK" "WB" "WH" "WR")
+             ("WP" "WP" "WP" "WP" "WP" "WP" "WP" "WP")
+             ("--" "--" "--" "--" "--" "--" "--" "--")
+             ("--" "--" "--" "--" "--" "--" "--" "--")
+             ("--" "--" "--" "--" "--" "--" "--" "--")
+             ("--" "--" "--" "--" "--" "--" "--" "--")
+             ("BP" "BP" "BP" "BP" "BP" "BP" "BP" "BP")
+             ("BR" "BH" "BB" "BQ" "BK" "BB" "BH" "BR")))
 
 ;legend:
 ;king => K
@@ -22,8 +20,8 @@
 ;enpty space\tile => --
 
 ;prefix:
-;black piece => B
-;white piece => W
+;black piece => "B"
+;white piece => "W" 
 
 ;movement
 
@@ -38,11 +36,19 @@
 (define (possiblePawnMoves B color)
   (append (map (findPosOfAll B1 'BP 0 0))))
 
-(define (pawnMoves B Xpos Ypos side) ;side will invert the movement of the pawn (its the color...)
+;pawn section (its the only piece that gets one...)
+                       ;WIP
+(define (pawnMoves-startingLane B Xpos Ypos side) ;side will invert the movement of the pawn (its the color...)
   (cond
     ((= 1 side) (take (lookLine B Xpos Ypos) 2))  ;white pawn move
     (else (take (lookLine B Xpos Ypos 0 -1) 2)))) ;black pawn move
-    ;need to add kills (their diffrent from regular move
+    ;need to add kills (they are diffrent from regular move)
+
+;regular move - 1 tile
+;kills 1 diagonal left or rigth from movement direction
+;first move - 2 tiles
+;capture during first move
+;croned at the end of the board
     
 
 ;move options (NO KILLS... yet ;))
@@ -58,33 +64,31 @@
     ((IllLegalMove? B Xpos Ypos) '())
     (else (cons (list Xpos Ypos) (lookDiagonal B (+ Xpos Xchange) (+ Ypos Ychange) Xchange Ychange #F)))))
 
-(define (removeOriginTile list) ;just 'deletes' the furst entry of the list (the origin tile of the move)
-  (remove (first list) list))
 
 ;movement checks
 (define (IllLegalMove? B Xpos Ypos)
   (or (not (legalTile? B Xpos Ypos)) (not (emptyTile? B Xpos Ypos))))
 
 (define (emptyTile? B Xpos Ypos)
-  (equal? (tileAt B Xpos Ypos) '--))
+  (equal? (getTileAt B Xpos Ypos) "--"))
 
 (define (legalTile? B Xpos Ypos)
   (and (< Xpos 8) (< Ypos 8) (> Xpos -1) (> Ypos -1)))
 
+(define (getColor B Xpos Ypos)
+  (string-ref (getTileAt B Xpos Ypos) 0))
+
 ;printing
 (define (printBoard B) ;prints the board
-  (cond
-    ((empty? (rest B)) (print (first B)))
-    (else (print (first B))
-          (newline)
-          (printBoard (rest B)))))
+  (for-each (lambda (line) (for-each (lambda (x) (display x) (display " ")) line) (newline)) B))
+   
 
 ;board operations
 (define (findPosOfAll B target Xpos Ypos)
   (cond
     ((= Ypos 8) '())
     ((= Xpos 8) (findPosOfAll B target 0 (add1 Ypos)))
-    ((equal? (tileAt B Xpos Ypos) target) (cons (list Xpos Ypos) (findPosOfAll B target (add1 Xpos) Ypos)))
+    ((equal? (getTileAt B Xpos Ypos) target) (cons (list Xpos Ypos) (findPosOfAll B target (add1 Xpos) Ypos)))
     (else (findPosOfAll B target (add1 Xpos) Ypos))))
 
 (define (updateBoard B Xpos Ypos input)
@@ -102,5 +106,5 @@
     ((legalTile? B Xpos Ypos) (updateBoard B Xpos Ypos '-))
     (else (print '(invalid tile)))))
 
-(define (tileAt B Xpos Ypos) ;returnes a tile in a given location
+(define (getTileAt B Xpos Ypos) ;returnes a tile in a given location
   (list-ref (list-ref B Ypos) Xpos))
